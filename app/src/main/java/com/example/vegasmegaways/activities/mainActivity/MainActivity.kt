601685aae9c3362a276515e141261417b09ed6b8
Dublongold <123116973+Dublongold@users.noreply.github.com>
2023-08-22 18:29:25 +0300

    Added url saving.

--- app/src/main/java/com/example/vegasmegaways/activities/mainActivity/MainActivity.kt
+++ app/src/main/java/com/example/vegasmegaways/activities/mainActivity/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.vegasmegaways.activities.mainActivity
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
@@ -43,33 +44,48 @@ class MainActivity : AppCompatActivity() {
                 webView.settings.loadWithOverviewMode = value
                 webView.settings.allowUniversalAccessFromFileURLs = value
             }
-
+        fun startWebActivity(url: String) {
+            urlForWebView.url = url
+            startActivity(webIntent)
+        }
         lifecycleScope.launch(Dispatchers.IO) {
-            GetAccess.get(this@MainActivity, GetAccess.URL) { pusk, link ->
-//                if (pusk && link != null) {
-                if(true) {
-//                    urlForWebView.url = link
-                    urlForWebView.url = "https://google.com"
-                    startActivity(webIntent)
-                } else {
-                    supportFragmentManager.beginTransaction()
-                        .add(R.id.theFragmentContainer, MenuFragment())
-                        .commitAllowingStateLoss()
-                    onBackPressedDispatcher.addCallback(object: OnBackPressedCallback(true) {
-                        override fun handleOnBackPressed() {
-                            val first = supportFragmentManager.fragments.first()
-                            if(first is WaitingFragment || first is MenuFragment) {
-                                finish()
+            val spu = SharedPreferencesUrl(::getSharedPreferences)
+            val url = spu.url
+            if(url.isNullOrEmpty()) {
+                GetAccess.get(this@MainActivity, GetAccess.URL) { pusk, link ->
+                    if (pusk && link != null) {
+                        spu.url = link
+                        startWebActivity(link)
+                    } else {
+                        supportFragmentManager.beginTransaction()
+                            .add(R.id.theFragmentContainer, MenuFragment())
+                            .commitAllowingStateLoss()
+                        onBackPressedDispatcher.addCallback(object: OnBackPressedCallback(true) {
+                            override fun handleOnBackPressed() {
+                                val first = supportFragmentManager.fragments.first()
+                                if(first is WaitingFragment || first is MenuFragment) {
+                                    finish()
+                                }
+                                else {
+                                    supportFragmentManager.beginTransaction()
+                                        .replace(R.id.theFragmentContainer, MenuFragment())
+                                        .commitAllowingStateLoss()
+                                }
                             }
-                            else {
-                                supportFragmentManager.beginTransaction()
-                                    .replace(R.id.theFragmentContainer, MenuFragment())
-                                    .commitAllowingStateLoss()
-                            }
-                        }
-                    })
+                        })
+                    }
                 }
             }
+            else startWebActivity(url)
         }
+    }
+    class SharedPreferencesUrl(private val getSharedPreferences: (String, Int) -> SharedPreferences) {
+        private val filename = "connection_url"
+        private val propertyName = "url"
+        private val modePrivate = AppCompatActivity.MODE_PRIVATE
+
+        var url
+            get() = getSharedPreferences(filename, modePrivate).getString(propertyName, "")
+            set(value) = getSharedPreferences(filename, modePrivate).edit().putString(propertyName, value).apply()
     }
 }
